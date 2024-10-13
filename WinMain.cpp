e972a5419e8df75b8b65e9d1fd30bdc62359c424
Pedro Ferreirinha <pedrompf99@gmail.com>
2024-10-13 21:13:31 +0100

    Defining the main window

--- WinMain.cpp
+++ WinMain.cpp
@@ -6,8 +6,32 @@ int CALLBACK WinMain(
 	LPSTR		lpCmdLine,
 	int			nCmdShow)
 {
-	while (true) {
+	const wchar_t pClassName[] = L"GraphicsEngine";
 
-	}
+	// register window class
+	WNDCLASSEX wc = { 0 };
+	wc.cbSize = sizeof(wc);
+	wc.style = CS_OWNDC;
+	wc.lpfnWndProc = DefWindowProc;
+	wc.cbClsExtra = 0;
+	wc.cbWndExtra = 0;
+	wc.hInstance = hInstance;
+	wc.hIcon = nullptr;
+	wc.hCursor = nullptr;
+	wc.hbrBackground = nullptr;
+	wc.lpszMenuName = nullptr;
+	wc.lpszClassName = pClassName;
+	wc.hIconSm = nullptr;
+	RegisterClassEx(&wc);
+
+	// create window instance
+	HWND hWnd = CreateWindowEx(
+		0, pClassName, L"Happy Window",
+		WS_CAPTION | WS_MINIMIZEBOX | WS_SYSMENU,
+		200, 200, 640, 480,
+		nullptr, nullptr, hInstance, nullptr
+	);
+
+	ShowWindow(hWnd, SW_SHOW);
 	return 0;
 }
