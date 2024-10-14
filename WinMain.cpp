3582aa5189aca885721c6fdff6555ff6babcabf5
Pedro Ferreirinha <pedrompf99@gmail.com>
2024-10-14 23:53:18 +0100

    Exit message on closing window

--- WinMain.cpp
+++ WinMain.cpp
@@ -1,5 +1,16 @@
 #include <Windows.h>
 
+LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam) 
+{
+	switch (msg)
+	{
+		case WM_CLOSE:
+			PostQuitMessage(420);
+			break;
+	}
+	return DefWindowProc(hWnd, msg, wParam, lParam);
+}
+
 int CALLBACK WinMain(
 	HINSTANCE	hInstance,
 	HINSTANCE	hPrevInstance,
@@ -12,7 +23,7 @@ int CALLBACK WinMain(
 	WNDCLASSEX wc = { 0 };
 	wc.cbSize = sizeof(wc);
 	wc.style = CS_OWNDC;
-	wc.lpfnWndProc = DefWindowProc;
+	wc.lpfnWndProc = WndProc;
 	wc.cbClsExtra = 0;
 	wc.cbWndExtra = 0;
 	wc.hInstance = hInstance;
@@ -32,6 +43,18 @@ int CALLBACK WinMain(
 		nullptr, nullptr, hInstance, nullptr
 	);
 
+	// show window
 	ShowWindow(hWnd, SW_SHOW);
-	return 0;
+
+	// message pump
+	MSG msg;
+	BOOL gResult;
+	while ( (gResult = GetMessage(&msg, nullptr, 0, 0)) > 0) {
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
+
+	if (gResult == -1)
+		return -1;
+	return msg.wParam;
 }
